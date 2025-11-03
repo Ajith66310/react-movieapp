9a37c9a6ca235f84f4828609c4ae856809acf69c
Ajith k v <ajith66310@gmail.com>
2025-11-03 11:52:35 +0530

    style: improve styling of Card and CardSkeleton components for better layout and consistency

--- frontend/src/components/CardSkeleton.jsx
+++ frontend/src/components/CardSkeleton.jsx
@@ -2,14 +2,17 @@ import React from "react";
 
 const CardSkeleton = () => {
   return (
-    <div className="bg-gray-900  rounded-xl overflow-hidden shadow-md animate-pulse">
+    <div
+      className="bg-gray-900 rounded-xl overflow-hidden shadow-md animate-pulse 
+                 flex flex-col h-full"
+    >
       {/* Poster Skeleton */}
       <div className="relative">
-        <div className="w-full h-120 bg-gray-800"></div>
+        <div className="w-full h-[350px] bg-gray-800"></div>
       </div>
 
       {/* Text Skeleton */}
-      <div className="p-4 space-y-3">
+      <div className="p-4 space-y-3 grow">
         <div className="h-5 bg-gray-700 rounded w-3/4"></div>
         <div className="h-4 bg-gray-700 rounded w-1/2"></div>
         <div className="h-3 bg-gray-700 rounded w-2/3"></div>
