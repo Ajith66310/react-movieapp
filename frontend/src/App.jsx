7812aebcc22a3b79d47513f1ecf1d0d5ca6517fe
Ajith k v <ajith66310@gmail.com>
2025-11-19 22:00:25 +0530

    fix: clerk navbar bug

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,60 +5,55 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import { Route, Routes } from "react-router-dom";
 import { MovieProvider } from "./contexts/MovieContext";
-import { SignedIn, SignedOut, RedirectToSignIn } from "@clerk/clerk-react";
+import { SignedIn, SignedOut, RedirectToSignIn, useUser } from "@clerk/clerk-react";
 import { Toaster } from "react-hot-toast";
 
 function App() {
+  const { isLoaded, isSignedIn } = useUser();
+
+  // Wait until Clerk session is loaded
+  if (!isLoaded) return null;
+
   return (
-    <>
-
-      <MovieProvider>
+    <MovieProvider>
       <Toaster position="top-right" />
-        <Navbar />
 
-        <main>
-          <Routes>
+      {/* Redirect immediately if user is signed out */}
+      <SignedOut>
+        <RedirectToSignIn />
+      </SignedOut>
 
-            {/* Public Routes */}
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+      {/* Show Navbar only for signed-in users */}
+      {isSignedIn && <Navbar />}
 
-            {/* Protected: Home */}
-            <Route
-              path="/"
-              element={
-                <>
-                  <SignedIn>
-                    <Home />
-                  </SignedIn>
+      <main>
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
 
-                  <SignedOut>
-                    <RedirectToSignIn />
-                  </SignedOut>
-                </>
-              }
-            />
+          {/* Protected: Home */}
+          <Route
+            path="/"
+            element={
+              <SignedIn>
+                <Home />
+              </SignedIn>
+            }
+          />
 
-            {/* Protected: Favourite */}
-            <Route
-              path="/favourite"
-              element={
-                <>
-                  <SignedIn>
-                    <Favourite />
-                  </SignedIn>
-
-                  <SignedOut>
-                    <RedirectToSignIn />
-                  </SignedOut>
-                </>
-              }
-            />
-
-          </Routes>
-        </main>
-      </MovieProvider>
-    </>
+          {/* Protected: Favourite */}
+          <Route
+            path="/favourite"
+            element={
+              <SignedIn>
+                <Favourite />
+              </SignedIn>
+            }
+          />
+        </Routes>
+      </main>
+    </MovieProvider>
   );
 }
 
