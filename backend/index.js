0ce2e2770488d8abca67835473c570717e62c102
Ajith k v <ajith66310@gmail.com>
2025-11-20 10:00:35 +0530

    refactor: cors to smartcors(testing)

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors";
+import smartcors from "smartcors";
 import movieRoutes from "./routes/movieRoutes.js";
 import connectDB from "./config/db.js";
 
@@ -9,11 +9,15 @@ dotenv.config();
 const app = express();
 
 
-app.use(cors({
-  origin: ["https://react-movieapp-alpha.vercel.app"],
-  credentials: true, // allows cookies/auth headers
-}));
-
+app.use(
+  smartcors({
+    allowedOrigins: [
+      "https://react-movieapp-alpha.vercel.app"
+    ],
+    allowCredentials: true,
+    debug: true,
+  })
+);
 
 
 app.use(express.json());
